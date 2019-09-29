bc80610f65060377d4fd3bb5909d1b128e2eca7d
= <=>
2019-09-29 17:16:37 +0200

    added build folder to client

--- api/resolvers/product.resolver.js
+++ api/resolvers/product.resolver.js
@@ -3,7 +3,6 @@ const products_data = require("../../data");
 module.exports = {
   Query: {
     products: (_, { limit = 6, offset = 0 }) => {
-      console.log(limit, offset);
       return products_data.slice(offset, offset + limit);
     },
     getProduct: (_, { model }) =>
---
+++ client/build/precache-manifest.b4874cbe05a7e65daf44cf87f2dfdff8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82c914f26329a8872854452c95802a81",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2dd03c8b28a969100a9b",
+    "url": "./static/css/2.bfd11837.chunk.css"
+  },
+  {
+    "revision": "9cabb1b7585bff195c8a",
+    "url": "./static/css/4.0319da5a.chunk.css"
+  },
+  {
+    "revision": "d7f188fc3f68cd4c0932",
+    "url": "./static/css/5.54fdb812.chunk.css"
+  },
+  {
+    "revision": "8a629c54fcc818023707",
+    "url": "./static/css/6.e7b0d155.chunk.css"
+  },
+  {
+    "revision": "3715dee40a91fb5b88b0",
+    "url": "./static/css/main.6ddc8bd3.chunk.css"
+  },
+  {
+    "revision": "2dd03c8b28a969100a9b",
+    "url": "./static/js/2.b7143e68.chunk.js"
+  },
+  {
+    "revision": "4df77ec1d6491f847e7b",
+    "url": "./static/js/3.1a9c985a.chunk.js"
+  },
+  {
+    "revision": "9cabb1b7585bff195c8a",
+    "url": "./static/js/4.8d9903c5.chunk.js"
+  },
+  {
+    "revision": "d7f188fc3f68cd4c0932",
+    "url": "./static/js/5.8e0b8327.chunk.js"
+  },
+  {
+    "revision": "8a629c54fcc818023707",
+    "url": "./static/js/6.337eefa3.chunk.js"
+  },
+  {
+    "revision": "0d25f2b436a2e7247045",
+    "url": "./static/js/7.2b5ce54a.chunk.js"
+  },
+  {
+    "revision": "3bedb6d8a2c174c9647f",
+    "url": "./static/js/8.d1ca5837.chunk.js"
+  },
+  {
+    "revision": "3715dee40a91fb5b88b0",
+    "url": "./static/js/main.4936e5ce.chunk.js"
+  },
+  {
+    "revision": "c661056cd9d15b80f875",
+    "url": "./static/js/runtime~main.5bba1dbe.js"
+  }
+]);
